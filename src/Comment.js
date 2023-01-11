b801f33ab1af6cb7eb828eeb5190c849c7339513
zaiky09 <zamilmozamil@outlook.com>
2023-01-11 07:34:41 +0300

    added comment 2

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with the opinion stipulated in this article.</div>;
+  return <div>
+          <p>Comment 1: Naturally, I agree with the opinion stipulated in this article.</p>
+          <p>Comment 2: I would second the opinion shared in comment 1.</p>
+        </div>;
 }
 
 export default Comment;
